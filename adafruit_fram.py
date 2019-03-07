14f9128870a5148fb951c0122c801417e223a8b1
Jerry Needell <jerryneedell@gmail.com>
2019-03-07 16:29:57 -0500

    fix issue#8 -- RPI  I/O error

--- adafruit_fram.py
+++ adafruit_fram.py
@@ -207,8 +207,8 @@ class FRAM_I2C(FRAM):
         dev_id_addr = 0xF8 >> 1
         read_buf = bytearray(3)
         with i2cdev(i2c_bus, dev_id_addr) as dev_id:
-            dev_id.write(bytearray([(address << 1)]), stop=False)
-            dev_id.readinto(read_buf)
+            dev_id.write_then_readinto(bytearray([(address << 1)]),
+                                       read_buf,stop=False)
         manf_id = (((read_buf[0] << 4) +(read_buf[1] >> 4)))
         prod_id = (((read_buf[1] & 0x0F) << 8) + read_buf[2])
         if (manf_id != _I2C_MANF_ID) and (prod_id != _I2C_PROD_ID):
