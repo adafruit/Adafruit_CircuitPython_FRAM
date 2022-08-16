8eb3604550f2eb17a6b1f68a73151edfe450079a
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_fram.py
+++ adafruit_fram.py
@@ -38,7 +38,7 @@ try:
 except ImportError:
     pass
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_FRAM.git"
 
 _MAX_SIZE_I2C = const(0x8000)
